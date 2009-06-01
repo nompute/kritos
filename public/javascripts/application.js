919447a763ce20398fd4dee7148c4089fac5fd16
Scott Blankenship <sblankenship@dhcp-172-19-253-61.mtv.corp.google.com>
2009-05-31 17:14:58 -0700

    Layout commits.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,25 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+// UsersTable constructor
+// Creates a table for a CSR's issue queue
+
+function UsersTable(tableId) {
+  this.table = document.getElementById(tableId);
+  this.rows = document.getElementsByTagName('tr');
+  this.userBaseUrl = '/users/';
+  this.setRowListeners();
+}
+
+UsersTable.prototype.setRowListeners = function() {
+  for(var row, i=0; row=this.rows[i]; i++) {
+    goog.events.listen(row, 'click', goog.bind(this.handleIssueClick, 
+                                               this,
+                                               row.id));
+  }
+}
+
+UsersTable.prototype.handleIssueClick = function(issueId) {
+  destinationUrl = this.issueBaseUrl + issueId;
+  document.location = destinationUrl;
+}
