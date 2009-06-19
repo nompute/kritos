ac8da9088225928faa99834e7d4a528012f12a7d
Michael Lai <myllai@gmail.com>
2009-06-19 02:14:35 -0700

    Misc changes to views.

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,6 @@
 class AdminController < ApplicationController
   before_filter :login_required
+  require_role :admin 
 
   def index
     if session[:user_id] == nil
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,11 @@ class SessionsController < ApplicationController
       self.current_user = user
       new_cookie_flag = (params[:remember_me] == "1")
       handle_remember_cookie! new_cookie_flag
-      redirect_back_or_default('/')
+      if user.has_role?("admin")
+        redirect_to "/admin"
+      else
+        redirect_back_or_default('/')
+      end
       flash[:notice] = "Logged in successfully"
     else
       note_failed_signin
